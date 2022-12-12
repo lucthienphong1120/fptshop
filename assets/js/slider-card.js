54c9a3745b0924bec6b58f5786bf5329c11f29ea
Lục Thiên Phong <chung.bkc13061@student.bkacad.edu.vn>
2022-12-12 21:17:46 +0700

    update

--- assets/js/slider-card.js
+++ assets/js/slider-card.js
@@ -2,29 +2,29 @@ window.addEventListener("load", function(){
 
 
 
-    const silderMain = document.querySelector('.silder-main');
-    const cardSilder = document.querySelectorAll('.card-silder');
-    const widthCardSlider = cardSilder[0].offsetWidth;
-    const lenghtCardSlider = cardSilder.length;
-    console.log(cardSilder);
+    const sliderMain = document.querySelector('.slider-main');
+    const cardslider = document.querySelectorAll('.card-slider');
+    const widthCardSlider = cardslider[0].offsetWidth;
+    const lenghtCardSlider = cardslider.length;
+    console.log(cardslider);
     // let sumCard = (widthCardSlider*lenghtCardSlider)/2;
     // console.log(sumCard)
     let tranX = 0; 
-    const backSilder = document.querySelector('.back-silder-card');
-    const nextSilder = document.querySelector('.next-silder-card');
+    const backslider = document.querySelector('.back-slider-card');
+    const nextslider = document.querySelector('.next-slider-card');
     let index = 0;
-    changenextSilder = nextSilder.onclick = function(){
+    changenextslider = nextslider.onclick = function(){
         
         if(index >= lenghtCardSlider-4) {
             
             index = 0;
-            silderMain.style = `transform: translateX(0px)`;
+            sliderMain.style = `transform: translateX(0px)`;
             tranX = 0;
             return;
         }
         // }if else(index < 1){return;}
         tranX = tranX - widthCardSlider;
-        silderMain.style = `transform: translateX(${tranX}px)`;
+        sliderMain.style = `transform: translateX(${tranX}px)`;
         // console.log(index);
         index++;
         console.log(index)
@@ -32,37 +32,37 @@ window.addEventListener("load", function(){
     let mob_view = window.matchMedia("(max-width: 739px)");
     	if (mob_view.matches)
     	 {
-            changenextSilder = nextSilder.onclick = function(){
+            changenextslider = nextslider.onclick = function(){
         
                 if(index >= lenghtCardSlider-2) {
                     
                     index = 0;
-                    silderMain.style = `transform: translateX(0px)`;
+                    sliderMain.style = `transform: translateX(0px)`;
                     tranX = 0;
                     return;
                 }
                 // }if else(index < 1){return;}
                 tranX = tranX - widthCardSlider;
-                silderMain.style = `transform: translateX(${tranX}px)`;
+                sliderMain.style = `transform: translateX(${tranX}px)`;
                 // console.log(index);
                 index++;
             }
     	 }
-    changebackSilder = backSilder.onclick = function(){
+    changebackslider = backslider.onclick = function(){
     
         if(index <1) {
             index = 0;
             return;
         }
         tranX = tranX + widthCardSlider;
-        silderMain.style = `transform: translateX(${tranX}px)`;
+        sliderMain.style = `transform: translateX(${tranX}px)`;
         // console.log(index);
         index--;
     }
     
                 
         // setInterval(function(){
-        setInterval(changenextSilder,5000);
+        setInterval(changenextslider,5000);
         // }, 1000);
     
     
--- assets/js/slider.js
+++ assets/js/slider.js
@@ -1,41 +1,35 @@
-window.addEventListener("load", function(){
-    const mainSilder = document.querySelector('.main-silder')
-    const itemSilder = document.querySelectorAll('.wrapper-item-silder')
-    console.log(itemSilder)
-    var withItem = itemSilder[0].offsetWidth;
+window.addEventListener("load", function () {
+    const mainslider = document.querySelector('.main-slider')
+    const itemslider = document.querySelectorAll('.wrapper-item-slider')
+    console.log(itemslider)
+    var withItem = itemslider[0].offsetWidth;
 
-    var lenghtItem = itemSilder.length;
-    let tranX = 0; 
+    var lenghtItem = itemslider.length;
+    let tranX = 0;
     const back = document.querySelector('.back');
     const next = document.querySelector('.next');
     let index = 1;
     // console.log(lenghtItem);
 
-
-
-    changeNext = next.onclick = function(){
-
-        if(index >= lenghtItem){
+    changeNext = next.onclick = function () {
+        if (index >= lenghtItem) {
             index = 1;
-            mainSilder.style = `transform: translateX(0px)`
+            mainslider.style = `transform: translateX(0px)`
             tranX = 0;
             return;
         }
-
         tranX = tranX - withItem;
-        mainSilder.style = `transform: translateX(${tranX}px)`
+        mainslider.style = `transform: translateX(${tranX}px)`
         index++;
         // console.log(tranX)
-
     }
-    changeback = back.addEventListener("click", function(){
-
-        if(index <= 1){
+    changeback = back.addEventListener("click", function () {
+        if (index <= 1) {
             index = 1;
             return;
         }
         tranX = tranX + withItem;
-        mainSilder.style = `transform: translateX(${tranX}px)`
+        mainslider.style = `transform: translateX(${tranX}px)`
         index--;
         // console.log(index)
 
@@ -51,7 +45,7 @@ window.addEventListener("load", function(){
 
 
     // })
-    
+
     // [...dot].forEach((item) =
     //     dot.addEventListener("click", function (e) {
     //       const slideIndex = parseInt(e.target.dataset.index);
